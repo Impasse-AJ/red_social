d74cd51787a8fa44274f06808b39951559ffcf2d
Impasse-AJ <abraham-paula@hotmail.com>
2025-01-31 09:25:40 +0100

    asd

--- redsocial.sql
+++ redsocial.sql
@@ -10,7 +10,7 @@ CREATE TABLE `usuarios` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nombre_usuario` VARCHAR(255) NOT NULL,
   `contrasena` VARCHAR(255) NOT NULL,
-  `fecha_creacion` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `fecha_creacion` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nombre_usuario` (`nombre_usuario`) 
 );
@@ -20,10 +20,9 @@ CREATE TABLE `publicaciones` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `id_usuario` INT NOT NULL,
   `contenido` VARCHAR(255) NOT NULL,
-  `fecha_creacion` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `fecha_creacion` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `id_usuario` (`id_usuario`),
-  CONSTRAINT `publicaciones_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
+  KEY `id_usuario` (`id_usuario`)
 );
 
 -- Estructura de tabla para la tabla `reacciones`
@@ -32,10 +31,25 @@ CREATE TABLE `reacciones` (
   `id_publicacion` INT NOT NULL,
   `id_usuario` INT NOT NULL,
   `tipo` VARCHAR(255) NOT NULL,
-  `fecha_creacion` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `fecha_creacion` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `id_publicacion` (`id_publicacion`),
-  KEY `id_usuario` (`id_usuario`),
-  CONSTRAINT `reacciones_ibfk_1` FOREIGN KEY (`id_publicacion`) REFERENCES `publicaciones` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `reacciones_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
+  KEY `id_usuario` (`id_usuario`)
 );
+
+-- Relaciones entre las tablas
+ALTER TABLE `publicaciones`
+  ADD CONSTRAINT `fk_publicaciones_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+
+ALTER TABLE `reacciones`
+  ADD CONSTRAINT `fk_reacciones_publicaciones` FOREIGN KEY (`id_publicacion`) REFERENCES `publicaciones` (`id`),
+  ADD CONSTRAINT `fk_reacciones_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+
+USE redsocial;
+
+-- Insertar usuarios con contraseñas hasheadas en la tabla `usuarios`
+INSERT INTO `usuarios` (`nombre_usuario`, `contrasena`, `fecha_creacion`)
+VALUES
+('abraham@social.com', PASSWORD('1234'), CURRENT_TIMESTAMP),
+('alejo@social.com', PASSWORD('1234'), CURRENT_TIMESTAMP),
+('alex@social.com', PASSWORD('1234'), CURRENT_TIMESTAMP);
