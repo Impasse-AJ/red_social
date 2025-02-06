492d881b2e2c52b7386841b3b05b1f94e12c66e0
Impasse-AJ <abraham-paula@hotmail.com>
2025-02-06 11:35:22 +0100

    testhome

---
+++ src/Controller/UsuariosController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Usuario;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class UsuariosController extends AbstractController
+{
+    #[Route('/usuarios', name: 'listar_usuarios')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')] // 🚀 Solo usuarios autenticados pueden ver la lista
+    public function listarUsuarios(EntityManagerInterface $entityManager): Response
+    {
+        // Obtener todos los usuarios de la base de datos
+        $usuarios = $entityManager->getRepository(Usuario::class)->findAll();
+
+        // Renderizar la plantilla y enviar los usuarios
+        return $this->render('usuarios.html.twig', [
+            'usuarios' => $usuarios
+        ]);
+    }
+}
